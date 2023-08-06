d9991556e97b1d8232c1d1b9fb2651b009adabf5
adut24 <dutertre.alexandre@laposte.net>
2023-08-06 18:58:16 +0200

    Add links between the spheres and transition anim

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,15 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Video;
 
+/// <summary>
+/// Represents the gestion of the current 360 video.
+/// </summary>
 public class VideoManager : MonoBehaviour
-{    
+{
+    public Animator fadeAnim;
+
+    /// <summary>
+    /// Changes to the sphere of the button pushed.
+    /// </summary>
+    /// <param name="destination">Sphere of the 360 video wanted.</param>
     public void ChangeSphere(GameObject destination)
     {
         GameObject currentSphere = FindObjectOfType<VideoPlayer>().gameObject;
-        Transform viewer = GameObject.Find("Viewer").transform;
+        StartCoroutine(StartNextVideo(currentSphere, destination));
+    }
 
+    /* Coroutine to load the next video during the transition animation. */
+    private IEnumerator StartNextVideo(GameObject currentSphere, GameObject destination)
+    {
+        fadeAnim.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1.5f);
+        currentSphere.GetComponentInChildren<Canvas>().enabled = false;
         destination.SetActive(true);
-        viewer.position = destination.transform.position;
+        transform.position = destination.transform.position;
+        while (!destination.GetComponentInChildren<VideoPlayer>().isPlaying)
+            yield return null;
+        ActivateCanvas(currentSphere, destination.GetComponentInChildren<Canvas>());
+    }
+
+    /* Activates the canvas of the current sphere. */
+    private void ActivateCanvas(GameObject currentSphere, Canvas destinationCanvas)
+    {
         currentSphere.SetActive(false);
+        destinationCanvas.enabled = true;
     }
 }
